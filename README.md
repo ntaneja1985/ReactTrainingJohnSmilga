d58e86efccbda463bbbb938ca5036adfc69cb719
ntaneja1985 <93190220+ntaneja1985@users.noreply.github.com>
2024-10-18 18:21:23 +0530

    Finished off coding for React Basics
    Finished off coding for React Basics

--- README.md
+++ README.md
@@ -82,6 +82,153 @@ const Author = () => {
 - In Inline styles general CSS rules apply. 
 - Inline Style always override CSS file styles
 
+# Children Prop
+- everything we render between component tags
+- during the course we will mostly use it Context API
+- special prop, has to be "children"
+- can place anywhere in JSX
+
+```js
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
+        </p>
+        <button>click me</button>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+    </section>
+  );
+}
+
+const Book = ({ img, title, author, children }) => {
+  // rest of the logic
+};
+const Book = (props) => {
+  const { img, title, author, children } = props;
+  console.log(props);
+  return (
+    <article className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+      {children}
+    </article>
+  );
+};
+```
+
+# Key Prop
+- The key prop in React is crucial for keeping your UI efficient and consistent. 
+- When rendering lists of elements, the key helps React identify which items have changed, been added, or removed. 
+- Without it, React would have to re-render the entire list every time something changes, which is not efficient.
+
+```js
+const items = ['Apple', 'Banana', 'Cherry'];
+
+const listItems = items.map((item, index) => 
+  <li key={index}>{item}</li>
+);
+
+return <ul>{listItems}</ul>;
+
+
+```
+- Each list item has a unique key prop, which helps React update the list efficiently.
+- Using indexes as keys can be a quick solution, but for more dynamic lists, unique identifiers are better to avoid bugs.
+
+# For copying objects use the spread operator
+
+```js
+const friends = ['john', 'peter', 'anna'];
+const newFriends = [...friends, 'susan'];
+console.log(friends);
+console.log(newFriends);
+const someObject = {
+  name: 'john',
+  job: 'developer',
+};
+// COPY NOT A REFERENCE !!!!
+const newObject = { ...someObject, location: 'florida' };
+console.log(someObject);
+console.log(newObject);
+
+```
+
+- Pass Data with help of spread operator:
+
+```js
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book  {...book} key={book.id} />;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+    </article>
+  );
+};
+const Book = ({ img, title, author }) => {
+  // rest of the code
+};
+
+```
+
+# Why do we do {()=>handleClick} instead of {handleClick}
+- This is a javascript feature {handleClick} is immediately invoked, but when we use anonymous function syntax it is invoked on the basis of an event like button click
+- Using {()=>handleClick} creates an anonymous function that calls handleClick, ensuring the function isn't invoked immediately during rendering. 
+- It passes a new function reference each time, which can cause unnecessary re-renders.
+- On the other hand, {handleClick} directly passes the function reference, calling it on the event, which is more efficient if the function doesn't require arguments or context.
+- Basically: {()=>handleClick} = safe but might re-render more, {handleClick} = efficient but use cautiously.
+
+### Import and Export Modules(ES6 Modules) help to organize our application
+
+### React optimizes images stored in the src folder
+
+***rem stands for "root em" and is a unit in CSS used for sizing elements relative to the root element's font size. Unlike em, which is relative to the font size of its parent element, rem is always relative to the root <html> element.***
+
+```js
+html {
+  font-size: 16px; /* Default root font size */
+}
+
+body {
+  font-size: 1rem; /* 16px */
+}
+
+h1 {
+  font-size: 2rem; /* 32px */
+}
+
+
+```
+- rem ensures consistency through our project
+
+## npm run build builds a production ready application
+
+
+
 
 
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,61 +1,95 @@
-import React, { useEffect } from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css' 
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 class BookObj {
-    constructor(image,title,author){
-        this.image = image;
-        this.title = title;
-        this.author = author;
-    }
+  constructor(id,image, title, author) {
+    this.id = id;
+    this.image = image;
+    this.title = title;
+    this.author = author;
+  }
 }
 
 function Greeting() {
-    return <>
-    <h2>My First Component</h2>
-    <ul>
+  return (
+    <>
+      <h2>My First Component</h2>
+      <ul>
         <li>
-            <a href='#'>hello world</a>
+          <a href="#">hello world</a>
         </li>
-    </ul>
+      </ul>
     </>
+  );
 }
 
-const BookList = () =>{
-    const bookList = [new BookObj("image1.jpg","Book1","Author1"), 
-                      new BookObj("image1.jpg","Book2","Author2"),
-                      new BookObj("image1.jpg","Book3","Author3")]
-    
-    useEffect(()=>{
-        document.title = "My First React Project"
-    },[])
-    return (
-        <section className='booklist'>
-           { bookList.map((book)=>{
-                return <Book image1={book.image} title={book.title} author={book.author}/>
-            })}
-        </section>
-    )
-}
+const BookList = () => {
+  const bookList = [
+    new BookObj(1,"image1.jpg", "Book1", "Author1"),
+    new BookObj(2,"image1.jpg", "Book2", "Author2"),
+    new BookObj(3,"image1.jpg", "Book3", "Author3"),
+  ];
 
-const Book = ({image1,title,author}) =>{
-    return (
-    <article className='book'>
-        <Image image1 = {image1}/>
-        <Title title = {title}/>
-        <Author author = {author}/>
-    </article> 
-    )
-}
+  const getBook = (id) => {
+    const book = bookList.find((book) => book.id === id);
+    console.log(book);
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const formValues = {};
+    data.forEach((value, key) => {
+      formValues[key] = value;
+    });
+    console.log(formValues)
+  };
 
-const Image = ({image1}) => {
-    let imageNew = './images/'+image1;
-    return (<img src={imageNew} alt = "testImage"/>)
-}
-const Title = ({title}) => <h2>{title}</h2>
-const Author = ({author}) => {
-    return <h4 style={{color:'#617d98',fontSize:'2rem', marginTop:'0.5rem'}}>{author.toUpperCase()}</h4>
-}
+  useEffect(() => {
+    document.title = "My First React Project";
+  }, []);
+  return (
+    <section className="booklist">
+      <EventExamples handleNewFormSubmit={handleFormSubmit} />
+      {bookList.map((book, index) => {
+        return (
+          <Book
+            key={index}
+            id = {book.id}
+            image1={book.image}
+            title={book.title}
+            author={book.author}
+            getBook={getBook}
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+const Book = ({ id, image1, title, author, getBook }) => {
+  return (
+    <article className="book">
+      <Image image1={image1} />
+      <Title title={title} />
+      <button onClick={()=>getBook(id)}>display title</button>
+      <Author author={author} />
+    </article>
+  );
+};
+
+const Image = ({ image1 }) => {
+  let imageNew = "./images/" + image1;
+  return <img src={imageNew} alt="testImage" />;
+};
+const Title = ({ title }) => <h2>{title}</h2>;
+const Author = ({ author }) => {
+  return (
+    <h4 style={{ color: "#617d98", fontSize: "2rem", marginTop: "0.5rem" }}>
+      {author.toUpperCase()}
+    </h4>
+  );
+};
 // function Greeting() {
 //     return React.createElement('h2',{},'hello world')
 // }
@@ -63,7 +97,43 @@ const Author = ({author}) => {
 // function Greeting() {
 //     return React.createElement('div',{},React.createElement('h2',{},'hello world'))
 // }
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const EventExamples = ({handleNewFormSubmit}) => {
+  const handleButtonClick = () => {
+    console.log("button clicked");
+  };
+//   const handleFormSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.target);
+//     const formValues = {};
+//     data.forEach((value, key) => {
+//       formValues[key] = value;
+//     });
+//     console.log(formValues)
+//   };
+  const handleFormChange = (event) =>{
+    // console.log(event);
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+  }
+  return (
+    <section>
+      <form onSubmit={handleNewFormSubmit}>
+        <h2>Form Input</h2>
+        <input
+          type="text"
+          name="example"
+          id="example"
+          onChange={handleFormChange}
+          style={{ margin: "1rem 0" }}
+        />
+        <button type="submit">Submit Form</button>
+      </form>
+      <button onClick={handleButtonClick}>Click Me!</button>
+    </section>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
